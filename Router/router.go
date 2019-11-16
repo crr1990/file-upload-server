ba7de9f1e2401adeab0563185f23a42f0e7e178c
eric.chen <eric.chen@dadaabc.com>
2019-11-16 19:09:20 +0800

    111

--- Router/router.go
+++ Router/router.go
@@ -3,7 +3,6 @@ package Router
 import (
 	"file-upload-server/Controllers"
 	"github.com/gin-gonic/gin"
-	"regexp"
 	"net/http"
 )
 
@@ -23,23 +22,13 @@ func InitRouter() {
 func CorsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
-		origin := c.Request.Header.Get("Origin")
-		var filterHost = [...]string{"http://*.hfjy.com"}
-		// filterHost 做过滤器，防止不合法的域名访问
-		var isAccess = false
-		for _, v := range(filterHost) {
-			match, _ := regexp.MatchString(v, origin)
-			if match {
-				isAccess = true
-			}
-		}
-		if isAccess {
+
 			// 核心处理方式
 			c.Header("Access-Control-Allow-Origin", "*")
 			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
 			c.Header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
 			c.Set("content-type", "application/json")
-		}
+
 		//放行所有OPTIONS方法
 		if method == "OPTIONS" {
 			c.JSON(http.StatusOK, "Options Request!")
